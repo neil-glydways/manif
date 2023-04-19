f2d6afcca9ac38ea8cbbbc0aef57dac1738c04b7
Neil Johnson <neil@glydways.com>
2023-04-19 10:26:28 -0600

    Update some header files with unused variables that threw warnings in glyd repo

--- include/manif/algorithms/average.h
+++ include/manif/algorithms/average.h
@@ -2,19 +2,20 @@
 #define _MANIF_MANIF_AVERAGE_H_
 
 #include "manif/impl/lie_group_base.h"
-//#include "manif/interpolation.h"
+// #include "manif/interpolation.h"
 #include <iostream>
+
 namespace manif {
 
-//template <template <typename LieGroup, typename...Args> class Container,
-//          typename LieGroup, typename...Args>
-//LieGroup
-//average_slerp(const Container<LieGroup, Args...>& mans)
+// template <template <typename LieGroup, typename...Args> class Container,
+//           typename LieGroup, typename...Args>
+// LieGroup
+// average_slerp(const Container<LieGroup, Args...>& mans)
 //{
-//  if (mans.empty())
-//    return LieGroup();
-//  else if (mans.size() == 1)
-//    return *mans.begin();
+//   if (mans.empty())
+//     return LieGroup();
+//   else if (mans.size() == 1)
+//     return *mans.begin();
 
 //  auto it = mans.begin();
 
@@ -48,225 +49,201 @@ namespace manif {
  * mass on compact Lie groups."
  * @link http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.721.8010&rep=rep1&type=pdf
  */
-template <template <typename LieGroup, typename...Args> class Container,
-          typename LieGroup, typename...Args>
-LieGroup
-average_biinvariant(const Container<LieGroup, Args...>& points,
-                    typename LieGroup::Scalar eps =
-                      Constants<typename LieGroup::Scalar>::eps,
-                    int max_iterations = 20)
-{
-  using Scalar  = typename LieGroup::Scalar;
-  using Tangent = typename LieGroup::Tangent;
+template<template<typename LieGroup, typename... Args> class Container, typename LieGroup, typename... Args>
+LieGroup average_biinvariant(const Container<LieGroup, Args...>& points,
+                             typename LieGroup::Scalar eps = Constants<typename LieGroup::Scalar>::eps,
+                             int max_iterations = 20) {
+    using Scalar = typename LieGroup::Scalar;
+    using Tangent = typename LieGroup::Tangent;
 
-  MANIF_CHECK(!points.empty(), "Points container is empty !");
-  if (points.size() == 1)
-    return *points.begin();
+    MANIF_CHECK(!points.empty(), "Points container is empty !");
+    if (points.size() == 1)
+        return *points.begin();
 
-  LieGroup avg = *points.begin();
+    LieGroup avg = *points.begin();
 
-  const Scalar w = Scalar(1) / Scalar(points.size());
+    const Scalar w = Scalar(1) / Scalar(points.size());
 
-  Tangent ts;
-  int i=0;
-  for (; i<max_iterations; ++i)
-  {
-    auto it        = points.begin();
-    const auto end = points.end();
+    Tangent ts;
+    int i = 0;
+    for (; i < max_iterations; ++i) {
+        auto it = points.begin();
+        const auto end = points.end();
 
-    ts.setZero();
-    for (; it != end; ++it)
-    {
-      // Update as in (a) & (b)
-      ts += ((*it) - avg);
+        ts.setZero();
+        for (; it != end; ++it) {
+            // Update as in (a) & (b)
+            ts += ((*it) - avg);
+        }
+        ts *= w; // doing the common product by 1/N just once
+
+        //////////////
+        // Stopping criterion is from (b)
+        //////////////
+
+        if (ts.coeffs().squaredNorm() < eps)
+            break;
+
+        avg += ts;
+
+        //////////////
+        // Stopping criterion is from (a)
+        //////////////
+
+        //    const LieGroup avg_0 = avg;
+        //    avg += ts;
+
+        //    if (avg.between(avg_0).log().coeffs().squaredNorm() < eps)
+        //      break;
     }
-    ts *= w; // doing the common product by 1/N just once
 
-    //////////////
-    // Stopping criterion is from (b)
-    //////////////
+    // std::cout << "Biinvariant stopped after " << i << " iterations.\n";
 
-    if (ts.coeffs().squaredNorm() < eps)
-      break;
-
-    avg += ts;
-
-    //////////////
-    // Stopping criterion is from (a)
-    //////////////
-
-//    const LieGroup avg_0 = avg;
-//    avg += ts;
-
-//    if (avg.between(avg_0).log().coeffs().squaredNorm() < eps)
-//      break;
-  }
-
-  //std::cout << "Biinvariant stopped after " << i << " iterations.\n";
-
-  return avg;
+    return avg;
 }
 
-template <template <typename LieGroup, typename...Args> class Container,
-          typename LieGroup, typename...Args>
-LieGroup
-average(const Container<LieGroup, Args...>& points,
-        typename LieGroup::Scalar eps =
-          Constants<typename LieGroup::Scalar>::eps,
-        int max_iterations = 20)
-{
-  using Scalar  = typename LieGroup::Scalar;
-  using Tangent = typename LieGroup::Tangent;
+template<template<typename LieGroup, typename... Args> class Container, typename LieGroup, typename... Args>
+LieGroup average(const Container<LieGroup, Args...>& points,
+                 typename LieGroup::Scalar eps = Constants<typename LieGroup::Scalar>::eps,
+                 int max_iterations = 20) {
+    using Scalar = typename LieGroup::Scalar;
+    using Tangent = typename LieGroup::Tangent;
 
-  MANIF_CHECK(!points.empty(), "Points container is empty !");
-  if (points.size() == 1)
-    return *points.begin();
+    MANIF_CHECK(!points.empty(), "Points container is empty !");
+    if (points.size() == 1)
+        return *points.begin();
 
-  LieGroup avg = *points.begin();
+    LieGroup avg = *points.begin();
 
-  const Scalar w = Scalar(1) / Scalar(points.size());
+    const Scalar w = Scalar(1) / Scalar(points.size());
 
-  Tangent ts, tmp;
-  typename LieGroup::Jacobian Jr, G;
-  for (int i=0; i<max_iterations; ++i)
-  {
-    auto it        = points.begin();
-    const auto end = points.end();
+    Tangent ts, tmp;
+    typename LieGroup::Jacobian Jr, G;
+    for (int i = 0; i < max_iterations; ++i) {
+        auto it = points.begin();
+        const auto end = points.end();
 
-    ts.setZero();
-    for (; it != end; ++it)
-    {
-      tmp = avg.between(*it).log();
+        ts.setZero();
+        for (; it != end; ++it) {
+            tmp = avg.between(*it).log();
 
-      // Neither (a) nor (b) use G for weighting
-      Jr = tmp.rjac();
-      G.noalias() = Jr.transpose() * Jr;
+            // Neither (a) nor (b) use G for weighting
+            Jr = tmp.rjac();
+            G.noalias() = Jr.transpose() * Jr;
 
-      ts += G * tmp;
+            ts += G * tmp;
+        }
+        ts *= w; // doing the common product by 1/N just once
+
+        // This stopping criterion is derived from (b)
+        typename LieGroup::Jacobian G = ts.rjac().transpose() * ts.rjac();
+        const Scalar n = ts.coeffs().transpose() * G * ts.coeffs();
+
+        if (n < eps)
+            break;
+
+        avg += ts;
     }
-    ts *= w; // doing the common product by 1/N just once
 
-    // This stopping criterion is derived from (b)
-    typename LieGroup::Jacobian G = ts.rjac().transpose() * ts.rjac();
-    const Scalar n = ts.coeffs().transpose() * G * ts.coeffs();
-
-    if (n < Constants<Scalar>::eps)
-      break;
-
-    avg += ts;
-  }
-
-  return avg;
+    return avg;
 }
 
 // ftp://ftp-sop.inria.fr/epidaure/Publications/Arsigny/arsigny_rr_biinvariant_mean.pdf
 // page 38
 // https://hal.inria.fr/hal-00938320/document#subsection.118
 // page 94
-template <template <typename LieGroup, typename...Args> class Container,
-          typename LieGroup, typename...Args>
-LieGroup
-average_frechet_left(const Container<LieGroup, Args...>& points,
-                     typename LieGroup::Scalar eps =
-                       Constants<typename LieGroup::Scalar>::eps,
-                     int max_iterations = 20)
-{
-  using Scalar  = typename LieGroup::Scalar;
-  using Tangent = typename LieGroup::Tangent;
+template<template<typename LieGroup, typename... Args> class Container, typename LieGroup, typename... Args>
+LieGroup average_frechet_left(const Container<LieGroup, Args...>& points,
+                              typename LieGroup::Scalar eps = Constants<typename LieGroup::Scalar>::eps,
+                              int max_iterations = 20) {
+    using Scalar = typename LieGroup::Scalar;
+    using Tangent = typename LieGroup::Tangent;
 
-  MANIF_CHECK(!points.empty(), "Points container is empty !");
-  if (points.size() == 1)
-    return *points.begin();
+    MANIF_CHECK(!points.empty(), "Points container is empty !");
+    if (points.size() == 1)
+        return *points.begin();
 
-  LieGroup avg = *points.begin();
+    LieGroup avg = *points.begin();
 
-  const Scalar w = Scalar(1) / Scalar(points.size());
+    const Scalar w = Scalar(1) / Scalar(points.size());
 
-  Tangent ts, tmp;
-  typename LieGroup::Jacobian Jl;
-  int i=0;
-  for (; i<max_iterations; ++i)
-  {
-    auto it        = points.begin();
-    const auto end = points.end();
+    Tangent ts, tmp;
+    typename LieGroup::Jacobian Jl;
+    int i = 0;
+    for (; i < max_iterations; ++i) {
+        auto it = points.begin();
+        const auto end = points.end();
 
-    ts.setZero();
-    const LieGroup avg_0 = avg;
+        ts.setZero();
+        const LieGroup avg_0 = avg;
 
-    for (; it != end; ++it)
-    {
-      tmp = (*it) - avg_0; // Log( Avg^-1 . Xi )
+        for (; it != end; ++it) {
+            tmp = (*it) - avg_0;     // Log( Avg^-1 . Xi )
 
-      Jl = avg_0.log().ljac(); // Jl(Avg)
+            Jl = avg_0.log().ljac(); // Jl(Avg)
 
-      ts += Jl * tmp * w;
+            ts += Jl * tmp * w;
+        }
+
+        // Avg = Avg . Exp( Jl^-1(Avg) . ts )
+        avg = avg_0 + (avg_0.log().ljacinv() * ts);
+
+        tmp = avg_0.log().ljac() * (avg - avg_0);
+
+        if (tmp.coeffs().squaredNorm() < eps)
+            break;
     }
 
-    // Avg = Avg . Exp( Jl^-1(Avg) . ts )
-    avg = avg_0 + ( avg_0.log().ljacinv() * ts );
+    // std::cout << "Frechet Left stopped after " << i << " iterations.\n";
 
-    tmp = avg_0.log().ljac() * (avg - avg_0);
-
-    if (tmp.coeffs().squaredNorm() < eps)
-      break;
-  }
-
-  //std::cout << "Frechet Left stopped after " << i << " iterations.\n";
-
-  return avg;
+    return avg;
 }
 
-template <template <typename LieGroup, typename...Args> class Container,
-          typename LieGroup, typename...Args>
-LieGroup
-average_frechet_right(const Container<LieGroup, Args...>& points,
-                      typename LieGroup::Scalar eps =
-                        Constants<typename LieGroup::Scalar>::eps,
-                      int max_iterations = 20)
-{
-  using Scalar  = typename LieGroup::Scalar;
-  using Tangent = typename LieGroup::Tangent;
+template<template<typename LieGroup, typename... Args> class Container, typename LieGroup, typename... Args>
+LieGroup average_frechet_right(const Container<LieGroup, Args...>& points,
+                               typename LieGroup::Scalar eps = Constants<typename LieGroup::Scalar>::eps,
+                               int max_iterations = 20) {
+    using Scalar = typename LieGroup::Scalar;
+    using Tangent = typename LieGroup::Tangent;
 
-  MANIF_CHECK(!points.empty(), "Points container is empty !");
-  if (points.size() == 1)
-    return *points.begin();
+    MANIF_CHECK(!points.empty(), "Points container is empty !");
+    if (points.size() == 1)
+        return *points.begin();
 
-  LieGroup avg = *points.begin();
+    LieGroup avg = *points.begin();
 
-  const Scalar w = Scalar(1) / Scalar(points.size());
+    const Scalar w = Scalar(1) / Scalar(points.size());
 
-  Tangent ts, tmp;
-  typename LieGroup::Jacobian Jr;
-  int i=0;
-  for (; i<max_iterations; ++i)
-  {
-    auto it        = points.begin();
-    const auto end = points.end();
+    Tangent ts, tmp;
+    typename LieGroup::Jacobian Jr;
+    int i = 0;
+    for (; i < max_iterations; ++i) {
+        auto it = points.begin();
+        const auto end = points.end();
 
-    ts.setZero();
-    const LieGroup avg_0 = avg;
+        ts.setZero();
+        const LieGroup avg_0 = avg;
 
-    for (; it != end; ++it)
-    {
-      tmp = it->lminus(avg_0); // Log( Xi . Avg^-1 )
+        for (; it != end; ++it) {
+            tmp = it->lminus(avg_0); // Log( Xi . Avg^-1 )
 
-      Jr = avg_0.log().rjac(); // Jr(Avg)
+            Jr = avg_0.log().rjac(); // Jr(Avg)
 
-      ts += Jr * tmp * w;
+            ts += Jr * tmp * w;
+        }
+
+        // Avg = Exp(Jr^-1(Avg) . ts) * Avg
+        avg = avg_0.lplus(avg_0.log().rjacinv() * ts);
+
+        tmp = avg_0.log().rjac() * (avg.lminus(avg_0));
+
+        if (tmp.coeffs().squaredNorm() < eps)
+            break;
     }
 
-    // Avg = Exp(Jr^-1(Avg) . ts) * Avg
-    avg = avg_0.lplus( avg_0.log().rjacinv() * ts );
+    // std::cout << "Frechet Right stopped after " << i << " iterations.\n";
 
-    tmp = avg_0.log().rjac() * (avg.lminus(avg_0));
-
-    if (tmp.coeffs().squaredNorm() < eps)
-      break;
-  }
-
-  //std::cout << "Frechet Right stopped after " << i << " iterations.\n";
-
-  return avg;
+    return avg;
 }
 
 } /* namespace manif */
--- include/manif/impl/traits.h
+++ include/manif/impl/traits.h
@@ -6,20 +6,23 @@
 
 namespace manif {
 
-template <typename Derived> struct LieGroupBase;
-template <typename Derived> struct TangentBase;
+template<typename Derived>
+struct LieGroupBase;
+template<typename Derived>
+struct TangentBase;
 
 namespace internal {
 
-//template <typename T>
-//inline constexpr bool constexpr_false() {
-//    return false;
-//}
+// template <typename T>
+// inline constexpr bool constexpr_false() {
+//     return false;
+// }
 
-template <typename T>
-struct constexpr_false : std::false_type {};
+template<typename T>
+struct constexpr_false : std::false_type { };
 
-template <typename T> struct traits;
+template<typename T>
+struct traits;
 
 /// @note the following is from the Eigen library
 /// here we say once and for all that traits<const T> == traits<T>
@@ -28,18 +31,20 @@ template <typename T> struct traits;
 /// template parameters on which T itself depends.
 /// For example, traits<Map<const T> > != traits<Map<T> >, but
 ///              traits<const Map<T> > == traits<Map<T> >
-template<typename T> struct traits<const T> : traits<T> {};
+template<typename T>
+struct traits<const T> : traits<T> { };
 
 /**
  * @brief LieGroupProperties, a traits for defining some
  * group properties. E.g. Space dimension, Degrees of Freedom ...
  */
-template <typename _LieGroupBase> struct LieGroupProperties;
+template<typename _LieGroupBase>
+struct LieGroupProperties;
 
 //! @brief A traits helper to cast
 //!
 //! @note e.g. template <typename NewScalar> using LieGroupTemplate = typename traitscast<LieGroup, NewScalar>::cast;
-template <typename _Class, typename _NewScalar>
+template<typename _Class, typename _NewScalar>
 struct traitscast;
 
 /**
@@ -47,53 +52,55 @@ struct traitscast;
  * @note given using FooDouble = Foo<double, Ts...>
  * using FooFloat = typename traitscast<FooDouble, float>::cast;
  */
-template <
-  template <typename, template<typename> class...> class _Class,
-  typename _Scalar,
-  typename _NewScalar,
-  template<typename> class ... Ts>
-struct traitscast<_Class<_Scalar, Ts...>, _NewScalar>
-{
-  using cast = _Class<_NewScalar, Ts...>;
+template<template<typename, template<typename> class...> class _Class,
+         typename _Scalar,
+         typename _NewScalar,
+         template<typename>
+         class... Ts>
+struct traitscast<_Class<_Scalar, Ts...>, _NewScalar> {
+    using cast = _Class<_NewScalar, Ts...>;
 };
 
 /**
  * @brief Specialization for Rn.
  */
-template <
-  template <typename, unsigned int> class _Class,
-  typename _NewScalar,
-  typename _Scalar,
-  unsigned int _Dim>
-struct traitscast<_Class<_Scalar, _Dim>, _NewScalar>
-{
-  using cast = _Class<_NewScalar, _Dim>;
+template<template<typename, unsigned int> class _Class, typename _NewScalar, typename _Scalar, unsigned int _Dim>
+struct traitscast<_Class<_Scalar, _Dim>, _NewScalar> {
+    using cast = _Class<_NewScalar, _Dim>;
 };
 
 //! @brief A traits for detecting AutoDiff scalar types
-template <typename Scalar> struct is_ad : std::integral_constant<bool, false> { };
+template<typename Scalar>
+struct is_ad : std::integral_constant<bool, false> { };
 
 //! @brief Has function 'rjacinv' traits
-template<class, typename T> struct has_rjacinv_impl : std::false_type {};
-template<typename T> struct
-has_rjacinv_impl<decltype( void(std::declval<T>().rjacinv()) ), T> : std::true_type {};
-template<typename T> struct has_rjacinv : has_rjacinv_impl<void, T> {};
+template<class, typename T>
+struct has_rjacinv_impl : std::false_type { };
+
+template<typename T>
+struct has_rjacinv_impl<decltype(void(std::declval<T>().rjacinv())), T> : std::true_type { };
+
+template<typename T>
+struct has_rjacinv : has_rjacinv_impl<void, T> { };
 
 //! @brief Has function 'ljacinv' traits
-template<class, typename T> struct has_ljacinv_impl : std::false_type {};
-template<typename T> struct
-has_ljacinv_impl<decltype( void(std::declval<T>().ljacinv()) ), T> : std::true_type {};
-template<typename T> struct has_ljacinv : has_ljacinv_impl<void, T> {};
+template<class, typename T>
+struct has_ljacinv_impl : std::false_type { };
 
+template<typename T>
+struct has_ljacinv_impl<decltype(void(std::declval<T>().ljacinv())), T> : std::true_type { };
+
+template<typename T>
+struct has_ljacinv : has_ljacinv_impl<void, T> { };
 
 /**
  * @brief intseq: std::integer_sequence-equivalent
  */
-template<int ... _I>
-struct intseq
-{
-  using value_type = int;
-  static constexpr std::size_t size() noexcept {return sizeof...(_I);}
+template<int... _I>
+struct intseq {
+    using value_type = int;
+
+    static constexpr std::size_t size() noexcept { return sizeof...(_I); }
 };
 
 /**
@@ -102,78 +109,88 @@ struct intseq
 template<typename _Seq1, typename _Seq2>
 struct intseq_join;
 
-template<template<int ...> class _IntSeq, int ... _I1, int ... _I2>
-struct intseq_join<_IntSeq<_I1...>, _IntSeq<_I2...>>
-{
-  using type = _IntSeq<_I1..., _I2...>;
+template<template<int...> class _IntSeq, int... _I1, int... _I2>
+struct intseq_join<_IntSeq<_I1...>, _IntSeq<_I2...>> {
+    using type = _IntSeq<_I1..., _I2...>;
 };
 
 /**
  * @brief generate intseq 0, 1, ..., N-1
  */
 template<std::size_t _N>
-struct make_intseq
-{
-  using type = typename intseq_join<
-    typename make_intseq<_N - 1>::type, intseq<_N - 1>
-  >::type;
+struct make_intseq {
+    using type = typename intseq_join<typename make_intseq<_N - 1>::type, intseq<_N - 1>>::type;
 };
 
 template<>
-struct make_intseq<0>
-{
-  using type = intseq<>;
+struct make_intseq<0> {
+    using type = intseq<>;
 };
 
-template <std::size_t _N>
+template<std::size_t _N>
 using make_intseq_t = typename make_intseq<_N>::type;
 
-template <typename T>
+template<typename T>
 constexpr T accumulate(T t) {
-  return t;
+    return t;
 }
 
-template <typename T, typename... Rest>
+template<typename T, typename... Rest>
 constexpr T accumulate(T t, Rest... rest) {
-  return t + accumulate(rest...);
+    return t + accumulate(rest...);
 }
 
-template <int N, int i, int j, int ...Args> struct compute_indices_gen {
-  static constexpr std::array<int, sizeof...(Args)+1> get() {
-    return compute_indices_gen<N-1, i+j, Args..., i+j>::get();
-  }
+template<int N, int i, int j, int... Args>
+struct compute_indices_gen {
+    static constexpr std::array<int, sizeof...(Args) + 1> get() {
+        return compute_indices_gen<N - 1, i + j, Args..., i + j>::get();
+    }
 };
-template <int i, int j, int ...Args> struct compute_indices_gen<1, i, j, Args...> {
-  static constexpr std::array<int, sizeof...(Args)+1> get() {
-    return { { 0, Args... } };
-  }
+
+template<int i, int j, int... Args>
+struct compute_indices_gen<1, i, j, Args...> {
+    static constexpr std::array<int, sizeof...(Args) + 1> get() {
+        return {
+            {0, Args...}
+        };
+    }
 };
+
 //! @brief Compute indices given block-sizes
-template <int... Args> constexpr std::array<int, sizeof...(Args)> compute_indices() {
-  return compute_indices_gen<sizeof...(Args), 0, Args...>::get();
+template<int... Args>
+constexpr std::array<int, sizeof...(Args)> compute_indices() {
+    return compute_indices_gen<sizeof...(Args), 0, Args...>::get();
 }
 
 // traits to check if a template parameter has a LieGroupBase base.
-template <typename Derived>
-void test_lie_group_base(LieGroupBase<Derived>&& s) {}
+template<typename Derived>
+void test_lie_group_base(LieGroupBase<Derived>&& s) {
+    (void)s;
+}
 
-template <class, typename T> struct is_manif_group_impl : std::false_type {};
-template <typename T> struct
-is_manif_group_impl<decltype(test_lie_group_base(std::declval<T>())), T>
-  : std::true_type {};
-template <typename T> struct is_manif_group
-  : is_manif_group_impl<void, T> {};
+template<class, typename T>
+struct is_manif_group_impl : std::false_type { };
+
+template<typename T>
+struct is_manif_group_impl<decltype(test_lie_group_base(std::declval<T>())), T> : std::true_type { };
+
+template<typename T>
+struct is_manif_group : is_manif_group_impl<void, T> { };
 
 // traits to check if a template parameter has a TangentBase base.
-template <typename Derived>
-void test_tangent_base(TangentBase<Derived>&& s) {}
+template<typename Derived>
+void test_tangent_base(TangentBase<Derived>&& s) {
+    (void)s;
+}
 
-template <class, typename T> struct is_manif_tangent_impl : std::false_type {};
-template <typename T> struct
-is_manif_tangent_impl<decltype(test_tangent_base(std::declval<T>())), T>
-  : std::true_type {};
-template <typename T> struct is_manif_tangent
-  : is_manif_tangent_impl<void, T> {};
+template<class, typename T>
+struct is_manif_tangent_impl : std::false_type { };
+
+template<typename T>
+struct is_manif_tangent_impl<decltype(test_tangent_base(std::declval<T>())), T> : std::true_type { };
+
+template<typename T>
+struct is_manif_tangent : is_manif_tangent_impl<void, T> { };
 
 } /* namespace internal */
 } /* namespace manif */
